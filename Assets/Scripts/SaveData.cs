cb5208ca671f2b99e53978c31d8b88956b9bc5a7
LobsterRoast <jekamminga@outlook.com>
2025-01-14 22:13:08 -0500

    Json stuff kind of working

--- Assets/Scripts/SaveData.cs
+++ Assets/Scripts/SaveData.cs
@@ -2,24 +2,44 @@ using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System;
-using System.Text.Json;
+using System.Text;
+using System.Text.Json.Nodes;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "SaveData", menuName = "Scriptable Objects/SaveData")]
 
 public class SaveData : ScriptableObject {
-    private JsonDocument json;
-    private JsonElement root;
+    // Save data is handled via json
+    // Yes, it's easily manipulatable so people could theoretically put themselves at the end of the game.
+    // That's the point. You paid for the game; if you wanna skip 90% of it, that's your perogative.
+    private JsonNode root;
     public string save_file_path;
-    // This could be done in one dictionary, but for faster loading, a separate hashmap is made for each scene.
-    private Dictionary<int, Saveable> save_data = new Dictionary<int, Saveable>();
-    public void AddSaveData(Saveable data) {
-        JsonElement data_element = new JsonElement();
-        if (!root.TryGetProperty(data.id.ToString(), out data_element)) {
-        }
+    private void EnsureSceneDataExists(int scene_number) {
+        Debug.Log(Application.persistentDataPath);
+        JsonObject root_object = root.AsObject();
+        string scene_number_string = scene_number.ToString();
+        if (!root_object.ContainsKey(scene_number_string))
+            root_object[scene_number_string] = new JsonObject();
     }
-
-    [ContextMenu("Commit Save Data")]
+    public void LoadJsonData(string file_name) {
+        save_file_path = Path.Combine(Application.persistentDataPath, file_name);
+        root = JsonNode.Parse(File.ReadAllText(save_file_path));
+    }
+    public void AddSaveData(Saveable data) {
+        LoadJsonData("Test.json");
+        EnsureSceneDataExists(data.scene_number);
+        string id_string = data.id.ToString();
+        string scene_number_string = data.scene_number.ToString();
+        if (!root[scene_number_string].AsObject().ContainsKey(id_string)) {
+            root[scene_number_string][id_string] = new JsonObject();
+        }
+        root[scene_number_string][id_string] = JsonNode.Parse(JsonUtility.ToJson(data, true));
+    }
     public void CommitSaveData() {
+        FileStream json_file = File.OpenWrite(save_file_path);
+        byte[] bytes = Encoding.UTF8.GetBytes(root.ToJsonString());
+        Debug.Log(root.ToJsonString());
+        json_file.Write(bytes);
+        json_file.Dispose();
     }
 }
