cf0b419fa0ef29d76d22f4985a96dbb646a47a2f
lobsterroast <jekamminga@outlook.com>
2025-01-19 13:40:54 -0500

    Added Accessory.cs

---
+++ Accessory.cs
@@ -0,0 +1,6 @@
+public enum Accessory {
+    None = 1 << 0,               // 0b00000001
+    Jetpack = 1 << 1,            // 0b00000010
+    Instantiator = 1 << 2,       // 0b00000100
+    Gravity_Manipulator = 1 << 3 // 0b00001000
+}
